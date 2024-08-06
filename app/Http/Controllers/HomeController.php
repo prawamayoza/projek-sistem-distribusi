8adf6bdb14bb73535e1badba797cbbc7f3378d27
prawamayoza <prawamayoza@gmail.com>
2024-08-06 19:47:49 +0700

    feat:dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Distribusi;
+use App\Models\Kendaraan;
+use App\Models\Pelanggan;
+use App\Models\Pesanan;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,8 +28,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home',[
-            'title' => 'CV.Bintang Berkah'
+        $login = auth()->user();
+
+        // Ambil data berdasarkan peran pengguna
+        if ($login->hasRole('driver')) {
+            $pesanan = Pesanan::whereDate('tanggal', now()->toDateString())->sum('total');
+        } else {
+            $pesanan = Pesanan::all()->sum('total');
+        }
+        $distribusi     = Distribusi::where('status', 'Waiting')->count();
+        $user           = User::all()->count();
+        $pelanggan      = Pelanggan::all()->count();
+        $kendaraan      = Kendaraan::where('status', 'Available')->count();
+        
+        return view('home', [
+            'pesanan'       => $pesanan,
+            'distribusi'    => $distribusi,
+            'kendaraan'     => $kendaraan,
+            'user'          => $user,
+            'pelanggan'     => $pelanggan,
+            'title'         => 'CV.Bintang Berkah'
         ]);
+        
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,152 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="container position-relative">
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                    {{ __('You are logged in!') }}
+            <p>{{ __('You are logged in!') }}</p>
+            <br>
+            <div class="container-fluid py-4 mb-3">
+                <div class="row">
+                    @role('kepala gudang')
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">person</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total User</p>
+                                        <h4 class="mb-0">{{ $user }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">person</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total Pelanggan</p>
+                                        <h4 class="mb-0">{{ $pelanggan }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">shopping_basket</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total Pesanan</p>
+                                        <h4 class="mb-0">{{ $pesanan }} </h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">directions_car</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Kendaraan Available</p>
+                                        <h4 class="mb-0">{{ $kendaraan }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                    @endrole
+                    @role('driver')
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">shopping_basket</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total Pesanan Hari Ini</p>
+                                        <h4 class="mb-0">{{ $pesanan }} </h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">directions_car</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Kendaraan Available</p>
+                                        <h4 class="mb-0">{{ $kendaraan }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                    @endrole
+                    @role('manager')
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">shopping_basket</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total Pesanan</p>
+                                        <h4 class="mb-0">{{ $pesanan }} </h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">person</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Total Pelanggan</p>
+                                        <h4 class="mb-0">{{ $pelanggan }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 mb-4">
+                            <div class="card">
+                                <div class="card-header p-3 pt-2 position-relative">
+                                    <div class="icon icon-lg icon-shape bg-gradient-warning shadow-warning text-center border-radius-xl mt-n4 position-absolute top-0 start-0 translate-middle">
+                                        <i class="material-icons opacity-10">check_circle</i>
+                                    </div>
+                                    <div class="text-end pt-3">
+                                        <p class="text-sm mb-0 text-capitalize">Data Perhitungan Yang Belum Approve</p>
+                                        <h4 class="mb-0">{{ $distribusi }}</h4>
+                                    </div>
+                                </div>
+                                <hr class="dark horizontal my-0">
+                            </div>
+                        </div>
+                    @endrole
                 </div>
             </div>
         </div>
